0db77bc226e7909a034f5d73c78c25a3ec5c6454
MYN2997 <hawkinsdamien007@gmail.com>
2026-08-15 08:03:58 +0700

    clean: remove temporary test scripts, diffs, backups, and untrack local database

--- check_generated.py
+++
@@ -1,5 +0,0 @@
-import pymupdf
-doc = pymupdf.open('c:/Users/hawki/Documents/KerjaPraktik/InformedConsentTCM/storage/pdf/TCM_Consent_John_Doe_20260814_103954.pdf')
-for page in doc:
-    for w in page.widgets():
-        print(w.field_name, "->", w.field_value)
